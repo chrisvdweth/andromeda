dd10fa4283e98176bb5e096af64875999996e8aa
Christian von der Weth <vonderweth@nus.edu.sg>
2017-02-27 14:06:28 +0800

    Added yaml files

--- andromeda/config/configreader.py
+++ andromeda/config/configreader.py
@@ -7,7 +7,7 @@ class ConfigReader:
     CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
 
     def __init__(self, conf_file_name):
-        f = open(os.path.expanduser(conf_file_name))
+        f = open(conf_file_name)
         # use safe_load instead load
         self.data = yaml.safe_load(f)
         f.close()
